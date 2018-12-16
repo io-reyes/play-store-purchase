670cb53cb2fb2a7c89b579824fa8fec1d99ec72a
Irwin Reyes <ioreyes@icsi.berkeley.edu>
2018-12-16 15:29:43 -0800

    Handle Firefox profiles with proxy settings and Google accounts

--- autobuy.py
+++ autobuy.py
@@ -6,10 +6,12 @@ import time
 from selenium import webdriver
 
 logger = logging.getLogger(__name__)
+my_dir = os.path.dirname(os.path.realpath(__file__))
 
 def _parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('paidlist', help='Path to file containing list of paid apps to purchase, one package name per line')
+    parser.add_argument('profile', help='Path to Firefox profile already logged into the purchasing account')
     parser.add_argument('--verbose', action='store_true', help='Enable verbose logging')
 
     return parser.parse_args()
@@ -25,17 +27,18 @@ def _parse_paid_list(paid_file):
     logger.info('Read %d lines' % len(paid_list))
     return paid_list
 
-def _buy(paid_list):
-    my_dir = os.path.dirname(os.path.realpath(__file__))
+def _buy(paid_list, profile_dir):
     geckodriver = os.path.join(my_dir, 'geckodriver', 'geckodriver-linux64')
     logging.info('Using geckodriver %s' % geckodriver)
 
-    socks_proxy = 'socks://localhost:5777'
-    logging.info('Using SOCKS proxy %s' % socks_proxy)
+    profile = webdriver.FirefoxProfile(profile_directory=profile_dir)
+    logging.info('Using profile %s' % profile.path)
 
-    with webdriver.Firefox(executable_path=geckodriver, proxy=socks_proxy) as driver:
+    with webdriver.Firefox(executable_path=geckodriver, firefox_profile=profile) as driver:
         driver.get('https://ipchicken.com')
-        time.sleep(1000)
+        time.sleep(3)
+        driver.get('https://play.google.com')
+        time.sleep(3)
         driver.close()
 
 if __name__ == '__main__':
@@ -47,7 +50,7 @@ if __name__ == '__main__':
         logging.basicConfig()
 
     paid_list = _parse_paid_list(args.paidlist)
-    _buy(paid_list)
+    _buy(paid_list, args.profile)
 
 
 
