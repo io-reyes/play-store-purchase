cc0ff104c0a8fa2c784194f0c6f4e45724789037
Irwin Reyes <ioreyes@icsi.berkeley.edu>
2018-12-16 18:48:39 -0800

    Working purchase script

--- autobuy.py
+++ autobuy.py
@@ -7,6 +7,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.keys import Keys
 
 logger = logging.getLogger(__name__)
 my_dir = os.path.dirname(os.path.realpath(__file__))
@@ -15,6 +16,7 @@ def _parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('paidlist', help='Path to file containing list of paid apps to purchase, one package name per line')
     parser.add_argument('profile', help='Path to Firefox profile already logged into the purchasing account')
+    parser.add_argument('password', help='Password for the purchasing account')
     parser.add_argument('--verbose', action='store_true', help='Enable verbose logging')
 
     return parser.parse_args()
@@ -30,7 +32,7 @@ def _parse_paid_list(paid_file):
     logger.info('Read %d lines' % len(paid_list))
     return paid_list
 
-def _buy(paid_list, profile_dir):
+def _buy(paid_list, profile_dir, password, seconds_between=20):
     geckodriver = os.path.join(my_dir, 'geckodriver', 'geckodriver-linux64')
     logger.info('Using geckodriver %s' % geckodriver)
 
@@ -48,6 +50,10 @@ def _buy(paid_list, profile_dir):
                 logger.info('Clicking the first Buy button for %s' % app)
                 buy_button.click()
 
+                purchase_iframe = WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located((By.XPATH, "//iframe[starts-with(@src, 'https://play.google.com/store/epurchase')]")))
+                logger.info('Found the purchasing iframe')
+                driver.switch_to.frame(purchase_iframe)
+
                 continue_button = WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located((By.ID, 'purchase-ok-button')))
                 logger.info('Clicking the Continue button for %s' % app)
                 continue_button.click()
@@ -56,12 +62,17 @@ def _buy(paid_list, profile_dir):
                 logger.info('Clicking the second Buy button for %s' % app)
                 buy_button_2.click()
 
+                driver.switch_to.default_content()
+                password_field = WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located((By.XPATH, "//input[@type='password' and @aria-label='Enter your password']"))) 
+                logger.info('Entering the password')
+                password_field.send_keys(password)
+                password_field.send_keys(Keys.ENTER)
+
+                logger.info('Sleeping for %d seconds after successful purchase' % seconds_between)
+                time.sleep(seconds_between)
+
             except Exception as e:
                 logger.exception('Exception buying %s' % app)
-
-            finally:
-                logger.info('Sleeping for 2 seconds between pages')
-                time.sleep(2)
 
         driver.close()
 
@@ -74,5 +85,5 @@ if __name__ == '__main__':
         logging.basicConfig()
 
     paid_list = _parse_paid_list(args.paidlist)
-    _buy(paid_list, args.profile)
+    _buy(paid_list, args.profile, args.password)
 
